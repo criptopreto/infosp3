07ea9169e43ea98f149f48ec901b4e45add8f05c
criptopreto <criptopreto@gmail.com>
2020-12-05 22:04:01 -0400

    fix

--- web/static/realtime/js/index.js
+++ web/static/realtime/js/index.js
@@ -25,6 +25,8 @@ var opcionActiva = 1;
 var rangoActivo = [];
 var tituloActivo;
 
+var chart;
+
 window.iioArray = [];
 
 var finCarga = false;
@@ -369,42 +371,7 @@ var cargarIIO = (arrIIO, opciones={titulo_map:hoy})=>{
         tem_data.push(objTemp);
     }
 
-    // Themes begin
-    am4core.useTheme(am4themes_animated);
-    am4core.useTheme(am4themes_moonrisekingdom);
-    // Themes end
 
-    var chart = am4core.create("st_top_tie", am4charts.XYChart);
-    chart.padding(10, 20, 10, 10);
-
-    var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
-    categoryAxis.renderer.grid.template.location = 0;
-    categoryAxis.dataFields.category = "tie";
-    categoryAxis.renderer.minGridDistance = 1;
-    categoryAxis.renderer.inversed = true;
-    categoryAxis.renderer.grid.template.disabled = true;
-
-    var valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
-    valueAxis.min = 0;
-
-    var series = chart.series.push(new am4charts.ColumnSeries());
-    series.dataFields.categoryY = "tie";
-    series.dataFields.valueX = "cant";
-    series.tooltipText = "{valueX.value}"
-    series.columns.template.strokeOpacity = 0;
-    series.columns.template.column.cornerRadiusBottomRight = 5;
-    series.columns.template.column.cornerRadiusTopRight = 5;
-
-    var labelBullet = series.bullets.push(new am4charts.LabelBullet())
-    labelBullet.label.horizontalCenter = "left";
-    labelBullet.label.dx = 10;
-    labelBullet.label.text = "{values.valueX.workingValue.formatNumber('#as')}";
-    labelBullet.locationX = 0;
-
-    // as by default columns of the same series are of the same color, we add adapter which takes colors from chart.colors color set
-    series.columns.template.adapter.add("fill", function(fill, target){
-        return chart.colors.getIndex(target.dataItem.index);
-    });
 
     categoryAxis.sortBySeries = series;
 
@@ -477,6 +444,44 @@ $(document).ready(function () {
     });
 
     limpiarStorage();
+
+    chart = am4core.create("st_top_tie", am4charts.XYChart);
+
+    // Themes begin
+    am4core.useTheme(am4themes_animated);
+    am4core.useTheme(am4themes_moonrisekingdom);
+    // Themes end
+
+    chart.padding(10, 50, 10, 10);
+
+    var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
+    categoryAxis.renderer.grid.template.location = 0;
+    categoryAxis.dataFields.category = "tie";
+    categoryAxis.renderer.minGridDistance = 1;
+    categoryAxis.renderer.inversed = true;
+    categoryAxis.renderer.grid.template.disabled = true;
+
+    var valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
+    valueAxis.min = 0;
+
+    var series = chart.series.push(new am4charts.ColumnSeries());
+    series.dataFields.categoryY = "tie";
+    series.dataFields.valueX = "cant";
+    series.tooltipText = "{valueX.value}"
+    series.columns.template.strokeOpacity = 0;
+    series.columns.template.column.cornerRadiusBottomRight = 5;
+    series.columns.template.column.cornerRadiusTopRight = 5;
+
+    var labelBullet = series.bullets.push(new am4charts.LabelBullet())
+    labelBullet.label.horizontalCenter = "left";
+    labelBullet.label.dx = 10;
+    labelBullet.label.text = "{values.valueX.workingValue.formatNumber('#as')}";
+    labelBullet.locationX = 0;
+
+    // as by default columns of the same series are of the same color, we add adapter which takes colors from chart.colors color set
+    series.columns.template.adapter.add("fill", function(fill, target){
+        return chart.colors.getIndex(target.dataItem.index);
+    });
 
     socket = io(`${window.servidorNodeapi}`);
     document.addEventListener('click', function enableNoSleep() {
